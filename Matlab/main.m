6822c2e1b0b21585456aba86eac5075cfdcd659b
olssdani <daniel.olsson1992@gmail.com>
2018-02-01 15:57:37 +0100

    Fixat vinklar åt båda hållen

--- Matlab/main.m
+++ Matlab/main.m
@@ -10,36 +10,28 @@ n2 = 1.51;
 %            1.5, -sqrt(6.75)/2, 1;
 %           -1.5, -sqrt(6.75)/2, 1;
 %           0,sqrt(6.75)/2, 1]';
-% Prism_kord = [0, 1.875, 1;
-%            1.5, -1.1250, 1;
-%           -1.5, -1.1250, 1;
-%           0,1.875, 1]';  
+Prism_kord = [0, 2.5981-0.866, 0;
+           1.5, -0.866, 0;
+          -1.5, -0.866 0;
+          0,2.5981-0.866, 0]';  
       
-Prism_kord = [0, 2, 1;
-           2, -2, 1;
-          -2, -2, 1;
-          0,2, 1]';  
+% Prism_kord = [0, 2, 1;
+%            2, -2, 1;
+%           -2, -2, 1;
+%           0,2, 1]';  
 %% Rotation
 
-for i=0:0.01:2*pi
-
-    rotation = [cos(i) -sin(i) 0;
-         sin(i) cos(i) 0;
-         0 0 1]; 
+for i=0:1:360
+    i
+    rotation = [cosd(i) -sind(i) 0;
+        sind(i) cosd(i) 0;
+        0 0 1]; 
     Roterad = rotation*Prism_kord;
-    plot(Roterad(1,:),Roterad(2,:));
+   % plot(Roterad(1,:),Roterad(2,:));
     prism(n1,n2, Roterad);
-    axis([-6 6 -6 6]);
-    grid on
+    %axis([-6 6 -6 6]);
+    %grid 
+    prompt = '';
+    x = input(prompt)
     drawnow
-    %pause(5)
-   
 end
-
-
-% 
-% 
-% 
-% 
-
-% grid on
