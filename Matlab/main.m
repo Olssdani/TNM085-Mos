57c8bbf7b4127d96027782690679195df52c06af
Bapiro <lucas.rung@gmail.com>
2018-02-02 13:19:05 +0100

    rot_live

--- Matlab/main.m
+++ Matlab/main.m
@@ -12,12 +12,45 @@ Prism_kord = [0, 2.5981-0.866, 0;
 
 %% Rotation
 for i=0:1:360
-    rotation = [cosd(i) -sind(i) 0;
-        sind(i) cosd(i) 0;
-        0 0 1]; 
-    Roterad = rotation*Prism_kord;
-    prism(n1,n2, Roterad);
+   % rotation = [cosd(i) -sind(i) 0;
+    %    sind(i) cosd(i) 0;
+    %    0 0 1]; 
+    %Roterad = rotation*Prism_kord;
+    
+    for j = 1:1:4
+     [X(j),Y(j)] = rot_PosLive(Prism_kord(1, j), Prism_kord(2, j), -pi/20); 
+   end
+    Roterad = [X ; Y ; 0 0 0 0];
+    Prism_kord = [X ; Y ; 0 0 0 0];
+    
+    
+    %prism(n1,n2, Roterad);
    % prompt = '';
     %x = input(prompt)
     drawnow
+    plot( X, Y);
+    xlim([-5 5])
+    ylim([-5 5])
 end
+
+%%
+
+x = 1;
+y = -1; 
+X = zeros(1,360);
+Y = zeros(1,360);
+
+
+for j = 1:1:360;
+    
+   [X(j), Y(j)] = rot_PosLive(x, y, pi/20);
+   x = X(j);
+   y = Y(j);
+   
+   drawnow
+   plot(X,Y);
+    
+    
+end
+
+
